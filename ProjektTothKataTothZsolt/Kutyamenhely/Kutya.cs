b6b822ea771828cec0685adf893379f9e6210709
TothZsoltCkik <toth.zsolt@ckik.hu>
2025-05-28 12:26:53 +0200

    dsaasdas

---
+++ ProjektTothKataTothZsolt/Kutyamenhely/Kutya.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kutyamenhely
+{
+    internal class Kutya
+    {
+        private int kor;
+        private string nev;
+        private bool szobatisztaE;
+        private HashSet<string> tulajdonsagok;
+
+
+        public Kutya(int kor, string nev, bool szobatiszta, string tulajdonsagok)
+        {
+            this.kor = kor;
+            this.nev = nev;
+            this.szobatisztaE = szobatiszta;
+            this.tulajdonsagok = new HashSet<string>(tulajdonsagok.Split());
+        }
+
+        public Kutya(string valami)
+        {
+            string[] sor = valami.Split(';');
+            this.kor = int.Parse(sor[0]);
+            this.nev = sor[1];
+            this.szobatisztaE = bool.Parse(sor[2]);
+            this.tulajdonsagok = new HashSet<string>(sor[3].Split());
+        }
+        public string SzobatisztaE 
+        {
+            get
+            {
+                if (szobatisztaE)
+                {
+                    return "Szobatiszta";
+                }
+                else return "Nem szobatiszta";
+            }
+        }
+
+        public string Tulajdonsagok 
+        {
+            set
+            {
+                HashSet<string> tulajd = new HashSet<string> { "baratsagos", "huseges", "vedelmezo", "jatekos", "tanulekony" };
+                if (!tulajd.Contains(value)) return; // garbage collector
+                this.tulajdonsagok.Add(value);
+            }
+            get
+            {
+                string s = "";
+                foreach (string item in tulajdonsagok)
+                {
+                    s += item + " ";
+                }
+                return s;
+            }
+        }
+
+        public int Kor { get => kor; }
+
+        public override string ToString()
+        {
+            return $"{nev}, {kor} eves, {SzobatisztaE}, {Tulajdonsagok}";
+        }
+
+        
+    }
+}
---
+++ ProjektTothKataTothZsolt/Kutyamenhely/Menhely.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.WebRequestMethods;
+
+namespace Kutyamenhely
+{
+    internal class Menhely
+    {
+        private List<Kutya> kutyak;
+
+        public void FajlbolOlvas(string fajlnev)
+        {
+            StreamReader sr = new StreamReader(fajlnev);
+            while (!sr.EndOfStream)
+            {
+                string s = sr.ReadLine();
+                Kutya f = new Kutya(s);
+                kutyak.Add(f);
+            }
+            sr.Close();
+        }
+
+        public Kutya Nemtudom(string valasztas)
+        {
+            Kutya s = kutyak[0];
+            string veg = film != filmek[filmek.Count - 1] ? "," : ";";
+            for (int i = 0; i < kutyak.Count; i++)
+            {
+                if (kutyak[i].Kor < s.Kor)
+                {
+                    s = kutyak[i];
+                }
+            }
+        }
+    }
+}
